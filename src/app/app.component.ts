c3241bb2adb1bb446ad24e181940e369b547f535
Andrei Voicu <dreivoicu@gmail.com>
2020-07-14 20:51:07 +0300

    prepare

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  entities = [
+    { id: 1, name: 'Andrei Voicu', isAdmin: true },
+    { id: 2, name: 'John Due', isAdmin: false }
+  ];
+
+  controls!: FormArray;
+
+  ngOnInit(): void {
+    const toGroups = this.entities.map((entity: any) => {
+      return new FormGroup({
+        name: new FormControl(entity.name, Validators.required),
+        isAdmin: new FormControl(entity.isAdmin)
+      });
+    });
+
+    this.controls = new FormArray(toGroups);
+  }
+
+  getControl(index: number, field: string): AbstractControl | null {
+    return this.controls.at(index).get(field);
+  }
+}
